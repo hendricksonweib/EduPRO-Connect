12e9345b68107bb81ea5ff1c1cd572eec713485f
hendricksonweib <hendricksonrocha22@gmail.com>
2025-12-22 20:33:43 -0300

    feat: add cadastro de alunos

--- src/app/(main)/sidebar-data.ts
+++ src/app/(main)/sidebar-data.ts
@@ -1,9 +1,9 @@
 import {
     BookOpen,
-    DollarSign,
-    GraduationCap,
+    Calendar,
     LayoutDashboard,
-    ShieldCheck,
+    Settings,
+    Users,
 } from "lucide-react"
 
 export const sidebarData = {
@@ -18,26 +18,80 @@ export const sidebarData = {
             url: "/dashboard",
             icon: LayoutDashboard,
             isActive: true,
+            items: [
+                {
+                    title: "Desempenho",
+                    url: "/dashboard",
+                },
+                {
+                    title: "Resumo Financeiro",
+                    url: "/work-in-progress?section=financeiro-resumo",
+                },
+            ],
         },
         {
-            title: "Alunos e Turmas",
-            url: "/alunos-turmas",
-            icon: GraduationCap,
-        },
-        {
-            title: "Financeiro",
-            url: "/financeiro",
-            icon: DollarSign,
-        },
-        {
-            title: "Pedagógico",
-            url: "/pedagogico",
+            title: "Turmas",
+            url: "/work-in-progress?section=turmas",
             icon: BookOpen,
+            items: [
+                {
+                    title: "Matérias",
+                    url: "/work-in-progress?section=materias",
+                    items: [
+                        {
+                            title: "Conteúdos",
+                            url: "/work-in-progress?section=conteudos",
+                        }
+                    ]
+                },
+            ],
         },
         {
-            title: "Portaria",
-            url: "/portaria",
-            icon: ShieldCheck,
+            title: "Cadastros",
+            url: "/work-in-progress?section=cadastros",
+            icon: Users,
+            items: [
+                {
+                    title: "Alunos",
+                    url: "/cadastros/alunos",
+                },
+                {
+                    title: "Professores",
+                    url: "/work-in-progress?section=professores",
+                },
+            ],
+        },
+        {
+            title: "Calendário",
+            url: "/work-in-progress?section=calendario",
+            icon: Calendar,
+            items: [
+                {
+                    title: "Avisos",
+                    url: "/work-in-progress?section=avisos",
+                },
+            ],
+        },
+        {
+            title: "Administrativo",
+            url: "/work-in-progress?section=administrativo",
+            icon: Settings,
+            items: [
+                {
+                    title: "Financeiro",
+                    url: "/work-in-progress?section=financeiro",
+                },
+                {
+                    title: "Funcionários",
+                    url: "/work-in-progress?section=funcionarios",
+                    items: [
+                        {
+                            title: "Tipo de Acesso",
+                            url: "/work-in-progress?section=tipo-acesso",
+                        }
+                    ]
+                },
+            ],
         },
     ],
     navSecondary: [],
